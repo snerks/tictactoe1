421037c14ec8ac2e3cecd602847197f7210e6fc5
snerks <martin.github@outlook.com>
2017-02-06 18:51:56 +0000

    Working in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,116 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
+export interface AppProps {
 
-class App extends React.Component<null, null> {
+}
+
+export interface AppState {
+  playerOneSymbol: string;
+  playerTwoSymbol: string;
+  currentTurnSymbol: string;
+  board: string[];
+  winner: string | null;
+}
+
+const BLANK_SYMBOL = '?';
+const PLAYER_ONE_SYMBOL = 'Y';
+const PLAYER_TWO_SYMBOL = 'N';
+const DRAW_SYMBOL = 'Nobody';
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.setState(this.getInitialStateZZZ());
+  }
+
+  getInitialStateZZZ() {
+    return {
+      playerOneSymbol: PLAYER_ONE_SYMBOL,
+      playerTwoSymbol: PLAYER_TWO_SYMBOL,
+      currentTurnSymbol: PLAYER_ONE_SYMBOL,
+      board: [
+        BLANK_SYMBOL, BLANK_SYMBOL, BLANK_SYMBOL,
+        BLANK_SYMBOL, BLANK_SYMBOL, BLANK_SYMBOL,
+        BLANK_SYMBOL, BLANK_SYMBOL, BLANK_SYMBOL,
+      ],
+      winner: null,
+    };
+  }
+
+  handleClick(index: number) {
+
+    if (this.state.winner) {
+      return;
+    }
+
+    const selectedCellCurrentValue = this.state.board[index];
+
+    if (selectedCellCurrentValue !== BLANK_SYMBOL) {
+      return;
+    }
+
+    this.state.board[index] = this.state.currentTurnSymbol;
+
+    this.setState({
+      board: this.state.board,
+      currentTurnSymbol: (
+        this.state.currentTurnSymbol === this.state.playerOneSymbol ?
+          this.state.playerTwoSymbol :
+          this.state.playerOneSymbol
+      ),
+      winner: this.checkForWinner(),
+    });
+  }
+
+  checkForWinner(): string | null {
+    const board = this.state.board;
+
+    const winningComboIndices = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    const foundWinningCombo = winningComboIndices.find((combo: number[]) => {
+      if (
+        board[combo[0]] !== BLANK_SYMBOL &&
+        // board[combo[1]] !== '' &&
+        // board[combo[2]] !== '' &&
+        board[combo[0]] === board[combo[1]] &&
+        board[combo[1]] === board[combo[2]]) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    if (foundWinningCombo) {
+      return this.state.currentTurnSymbol;
+    }
+
+    const emptyCells = this.state.board.filter((cell: string) => cell === BLANK_SYMBOL);
+
+    return emptyCells.length === 0 ? DRAW_SYMBOL : null;
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app-container">
+        {this.state.winner ? <h1>{`The winner is ${this.state.winner}`}</h1> : null}
+        <div className="board">
+          {this.state.board.map((cell, index) => {
+            return <div key={index} onClick={() => this.handleClick(index)} className="square">{cell}</div>;
+          })}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
       </div>
     );
   }
